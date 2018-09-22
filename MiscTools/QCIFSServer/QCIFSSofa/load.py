cf3734a46b62436bdbc7f1ddcfd404078be3c1b3
James <drjwcain@gmail.com>
2018-09-22 12:51:54 -0700

    python can load the db some morer

--- MiscTools/QCIFSServer/QCIFSSofa/load.py
+++ MiscTools/QCIFSServer/QCIFSSofa/load.py
@@ -10,22 +10,36 @@ def index(pathToSearch, format):
 
 
 url = "http://127.0.0.1:5984/music/"
-artist = "Bowie"
-path = "D:\\Music\\B\\Bowie 1966 - 1976\\David Bowie - Hunky Dory\\"
-names = index(path, "*.mp3")
 
-for name in names:
-    record = {
-        "Type": "Recording",
-        "Artist": artist,
-        'Name': name,
-        'Path': path,
-    }
-    # print(str(record))
-    # val = json.dumps(record)
-    # print(val)
+def folder(artist, path):
+    names = index(path, "*.mp3")
 
-    responce = requests.post(url, json=record)
-    print(str(responce))
+    for name in names:
+        record = {
+            "Type": "Recording",
+            "Artist": artist,
+            'Name': name,
+            'Path': path,
+        }
+        # print(str(record))
+        # val = json.dumps(record)
+        # print(val)
+
+        reply = requests.post(url, json=record)
+        # print(str(reply.status_code))
 
 
+
+def masterFolder(artist, path):
+    files = glob.glob(path)
+    files.sort()
+    # names = [os.path.basename(f) for f in files]
+    names = files
+    print(str(names))
+
+    for name in names:
+        folder(artist, name + "\\")
+
+masterFolder("Bowie", "D:\\Music\\B\\Bowie 1966 - 1976\\*")
+
+masterFolder("Hawkwind", "D:\\Music\\H\\Hawkwind\\*")
