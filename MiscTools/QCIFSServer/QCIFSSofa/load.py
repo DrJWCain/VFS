a7a9dceccbd270192af48a8ed7c5a901e871305f
James <drjwcain@gmail.com>
2018-09-23 07:59:02 -0700

    License and tidy

--- MiscTools/QCIFSServer/QCIFSSofa/load.py
+++ MiscTools/QCIFSServer/QCIFSSofa/load.py
@@ -1,10 +1,28 @@
+# Copyright 2018 Grass Valley, A Belden Brand
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import glob, os, json, requests
 
 def index(pathToSearch, format):
-    files = glob.glob(os.path.join(pathToSearch, format))
+    print(pathToSearch)
+    search = os.path.join(pathToSearch, format)
+    print(search)
+    files = glob.glob(search)
+    print(files)
     files.sort()
     names = [os.path.basename(f) for f in files]
-    print(str(names))
+    print(names)
     return names
 
 
@@ -40,6 +58,8 @@ def masterFolder(artist, path):
     for name in names:
         folder(artist, name + "\\")
 
+# folder("Motorhead", "D:\\Music\\M\\Motorhead - The Best Of Greatest Hits\\")
 masterFolder("Bowie", "D:\\Music\\B\\Bowie 1966 - 1976\\*")
-
 masterFolder("Hawkwind", "D:\\Music\\H\\Hawkwind\\*")
+masterFolder("Led Zeplin", "D:\\Music\\L\\Led Zeppelin\\*")
+
